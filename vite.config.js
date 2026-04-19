de5682e0193c7719a576b43f8278501a5fa244a4
Cagil Disbudak <cdisbudak07@gmail.com>
2026-04-19 13:42:34 +0300

    fix: update base path and script source to support GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // GitHub Pages için gerekli (Relative paths)
+  base: '/ai-news/', // GitHub Pages repository ismi
 })
